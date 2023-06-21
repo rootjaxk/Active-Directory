b96760b349baa0af7998f15049842a1e6e2bf134
Jack <87831546+rootjaxk@users.noreply.github.com>
2023-06-21 20:15:42 +0100

    Add files via upload

---
+++ Remove-EmptyGroups.ps1
@@ -0,0 +1,43 @@
+﻿Import-Module ActiveDirectory
+
+# Find total amount of groups
+Write-Output "Finding total number of groups..."
+$totalgroups = (Get-ADGroup -filter *).count
+Write-Output "There are $totalgroups groups within the domain"
+
+# Get empty groups
+Write-Output "Finding total number of empty groups (may take a while) ..."
+$emptygroups = Get-ADGroup -Filter { Members -notlike "*" } | Select-Object Name, DistinguishedName
+
+# Find total amount of groups that are empty
+$totalempty = $emptygroups.count
+
+# Check if there are no empty groups (if script already executed)
+if ($totalempty -eq 0) {
+  Write-Output "There are no empty groups!"
+  return
+} else {
+  Write-Output "There are $totalempty empty groups within the domain"
+}
+
+# Create export path as users home directory
+$ExportPath = Join-Path $env:USERPROFILE "EmptyGroups.csv"
+
+# Export empty group list to csv
+$emptygroups | Export-Csv $ExportPath -NoTypeInformation
+Write-Output "Exported a list of all empty groups to $ExportPath"
+
+# confrm whether want to delete empty groups - deletion will require domain admin
+$confirmation = Read-Host "Do you want to delete all $totalempty groups? - requires domain admin privileges (Y/N)"
+
+if ($confirmation.ToUpper() -eq 'Y') {
+  # loop to cover all empty groups
+  ForEach ($Item in $emptygroups) {
+    Remove-ADGroup -Identity $Item.DistinguishedName -Confirm:$false
+    Write-Output "$($Item.DistinguishedName) - Deleted"
+  }
+  Write-Output "All empty groups deleted!"
+} 
+else {
+  Write-Output "Empty groups not deleted, review $ExportPath"
+}
---
+++ Remove-InactiveUsers.ps1
@@ -0,0 +1,60 @@
+﻿Import-Module ActiveDirectory
+
+# Find total number of users
+Write-Output "Finding total number of users..."
+$totalusers = (Get-ADUser -filter *).count
+Write-Output "There are $totalusers users within the domain"
+
+# Set the number of days since last logon
+$DaysInactive = 90
+$InactiveDate = (Get-Date).Adddays(-($DaysInactive))
+
+# Find stale users (will also find never logged on users)
+$staleusers = Search-ADAccount -AccountInactive -DateTime $InactiveDate -UsersOnly | Select-Object @{ Name="Username"; Expression={$_.SamAccountName} }, Name, LastLogonDate, DistinguishedName
+
+# Find total number of users that are stale
+$totalstale = $staleusers.count
+
+# Check if there are no stale users (if script already executed)
+if ($totalstale -eq 0) {
+  Write-Output "There are no inactive users!"
+  return
+} else {
+  Write-Output "There are $totalstale inactive users within the domain"
+}
+
+# Create export path as users home directory
+$ExportPath = Join-Path $env:USERPROFILE "InactiveUsers.csv"
+
+# Export inactive user list to csv
+$staleusers | Export-Csv $ExportPath -NoTypeInformation
+Write-Output "Exported a list of all inactive users to $ExportPath"
+
+# confrm whether want to disable inactive users
+$disableconfirmation = Read-Host "Do you want to disable all $staleusers inactive users? - requires domain admin privileges (Y/N)"
+
+if ($disableconfirmation -eq 'Y') {
+  # Disable Inactive Users
+  ForEach ($Item in $staleusers){
+    Disable-ADAccount -Identity $Item.DistinguishedName
+    Write-Output "$($Item.DistinguishedName) - Disabled"
+    Get-ADUser -Filter { DistinguishedName -eq $Item.DistinguishedName } | Select-Object @{ Name="Username"; Expression={$_.SamAccountName} }, Name, Enabled
+  }
+  Write-Output "All inactive users disabled!"
+} else {
+  Write-Output "Inactive users not disabled, review $ExportPath"
+}
+
+# confrm whether want to delete inactive users
+$deletionconfirmation = Read-Host "Do you want to delete all $staleusers inactive users? - requires domain admin privileges (Y/N)"
+
+if ($deletionconfirmation.ToUpper() -eq 'Y') {
+  #delete inactive Users
+  ForEach ($Item in $staleusers){
+    Remove-ADUser -Identity $Item.DistinguishedName -Confirm:$false
+    Write-Output "$($Item.Username) - Deleted"
+  }
+  Write-Output "All inactive users deleted!"
+} else {
+  Write-Output "Inactive users not deleted, review $ExportPath"
+}
